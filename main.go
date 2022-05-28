fa87e13a1db7e87001f0041f51443e49c7a97589
heloisereinaviegas@gmail.com <heloisereinaviegas@gmail.com>
2022-05-28 14:42:58 +0530

    update , delete functions added

--- main.go
+++ main.go
@@ -35,15 +35,38 @@ func createDest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	//create id using rand func from math library
 	var dest Destination
-	json.NewDecoder(r.Body).Decode(dest) //decode values from request and store at dest
+	json.NewDecoder(r.Body).Decode(&dest) //decode values from request and store at dest
 	destinations = append(destinations, dest)
 	json.NewEncoder(w).Encode(destinations)
 
 }
 func updateDest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //get id from request
+
+	var dest Destination
+	json.NewDecoder(r.Body).Decode(&dest) //get dest vales to upadte and store at &dest
+
+	for index, item := range destinations {
+		if item.ID == params["id"] { ///if id matches request id then delete that dest from splice
+			destinations = append(destinations[:index], destinations[index+1:]...)
+			dest.ID = params["id"] //for new dest values reset the id to original id that was sent in params
+			destinations = append(destinations, dest)
+			json.NewEncoder(w).Encode(destinations)
+
+		}
+	}
 
 }
 func deleteDest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range destinations {
+		if item.ID == params["id"] {
+			destinations = append(destinations[:index], destinations[index+1:]...)
+			json.NewEncoder(w).Encode(params["id"])
+		}
+	}
 
 }
 
